f864db2dd9b8a391460fa0652fad5fc68f8c97de
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2024-09-11 21:46:27 -0500

    feat:contacts_app

--- src/app/features/contacts/contact.service.ts
+++ src/app/features/contacts/contact.service.ts
@@ -1,9 +1,37 @@
 import { inject, Injectable } from "@angular/core";
 import { DocumentData, Firestore,addDoc,collection,deleteDoc,doc, updateDoc,getDoc,DocumentReference,orderBy,query,collectionData } from "@angular/fire/firestore";
+import {Contact} from "./contact.interfaces";
 
 import { Observable } from "rxjs"; // Son objetos que emiten valores en el tiempo, y se utilizan para manejos de eventos asincrónicos.
+
+import {APP_CONSTANTS} from "@shared/constants";
+
 @Injectable({providedIn: 'root'})
 
 export class ContactService{
-  
+
+  private readonly _firestore = inject(Firestore); // DB Firestore, inyectamos el servicio Firestore con la base de datos de Firebase
+
+  private readonly _contactCollection = collection(this._firestore,APP_CONSTANTS.COLLECTION_NAME); // collection(db, 'cities');
+
+  // Método para crear un nuevo contacto
+  newContact(contact:Partial<Contact>):Promise<DocumentReference<DocumentData>>{
+    return addDoc(this._contactCollection,{
+      ...contact,
+      created:Date.now(),
+      updated:Date.now()
+    })
+  }
+  // Método para obtener un contacto por id
+
+  // Método para actualizar un contacto
+
+  // Método para eliminar un contacto
+
+  // Método para obtener todos los contactos
+  getAllContacts():Observable<Contact[]>{
+    const queryFn = query(this._contactCollection,orderBy('created','desc'));
+    return collectionData(queryFn,{idField:'id'}) as Observable<Contact[]>;
+  }
+
 }
---
+++ src/app/shared/constants.ts
@@ -0,0 +1,11 @@
+export const APP_CONSTANTS = {
+  COLLECTION_NAME:'contacts',
+  MESSAGES:{
+    CONTACT_CREATED:'Contacto registrado correctamente',
+    CONTACT_UPDATED:'Contacto actualizado correctamente',
+    CONTACT_DELETED:'Contacto eliminado correctamente',
+    CONFIRMATION_PROMPT:'¿Estás seguro de que deseas eliminar este contacto?',
+  },
+  COUNTRIES: ['Argentina', 'Brasil', 'Chile', 'Colombia', 'Ecuador', 'México', 'Perú', 'Uruguay', 'Venezuela'],
+  COUNTRIES_CODES: ['AR', 'BR', 'CL', 'CO', 'EC', 'MX', 'PE', 'UY', 'VE'],
+}
